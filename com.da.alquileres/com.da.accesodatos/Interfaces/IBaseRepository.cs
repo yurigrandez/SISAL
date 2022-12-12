2178ac5fa5e75dc05180740f80633d19b1a16f91
Yuri Grandez <yurigrandez@gmail.com>
2022-12-12 15:00:44 -0500

    Se modifico el nombre del proyecto de acceso a datos

--- com.da.alquileres/com.da.accesodatos/Interfaces/IBaseRepository.cs
+++ com.da.alquileres/com.da.accesodatos/Interfaces/IBaseRepository.cs
@@ -4,14 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace com.da.accesodatos.Interfaces
+namespace com.da.alquileres.accesodatos.Interfaces
 {
     public interface IBaseRepository<T>
     {
         Task<ICollection<T>> listarAsync();
-
         Task<T> buscarXId(int id);
-
         Task<T> buscarXString(string str);
         Task<T> agregarEntidad(T entidad);
         Task<T> actualizarEntidad(T entidad);
