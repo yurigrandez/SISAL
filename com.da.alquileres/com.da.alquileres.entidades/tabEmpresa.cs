25325be7ccbb98a5c6b380837a9e09f2fe870c15
Yuri Grandez <yurigrandez@gmail.com>
2022-12-12 14:55:53 -0500

    Se agrego proyecto Entidades y Acceso a Datos

---
+++ com.da.alquileres/com.da.alquileres.entidades/tabEmpresa.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.da.alquileres.entidades
+{
+    public class tabEmpresa : tabEntidadBase
+    {
+        
+        public string codigo { get; set; } = default!;
+        public string nombre { get; set; } = default!;
+        public string direccion { get; set; } = default!;
+        public string ruc { get; set; } = default!;
+        public string telefono { get; set; } = default!;
+        public string movil { get; set; } = default!;
+        public string nombreContacto { get; set; } = default!;
+        public string correoContacto { get; set; } = default!;
+        public string tlfContacto { get; set; } = default!;
+        public string movilContacto { get; set; } = default!;
+        public string imgEmpresa { get; set; } = default!;
+
+    }
+}
